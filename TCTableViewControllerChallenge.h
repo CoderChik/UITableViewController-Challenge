ec20e50f36c9937e715077632eefa72caece6dbb
Jamie Berry <jamieberry@Jamies-MacBook-Air.local>
2014-04-26 14:53:49 -0500

    create project

---
+++ TCTableViewControllerChallenge.h
@@ -0,0 +1,13 @@
+//
+//  TCTableViewControllerChallenge.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Jamie Berry on 4/26/14.
+//  Copyright (c) 2014 Jamie Berry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TCTableViewControllerChallenge : UITableViewController
+
+@end
